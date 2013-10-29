ef150a96b13c1184921209fb70f3c0e94e58ae59
Tatros <tatros@in.tum.de>
2013-10-29 12:01:53 +0100

    Basic Skeleton for Milestone 1.

--- src/ui/Application.java
+++ src/ui/Application.java
@@ -1,16 +1,43 @@
 package ui;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.WriterAppender;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Layout;
 
 public class Application {
 
-	private static final Logger logger = Logger.getLogger(Application.class);
-	// Test test
-	public static void main(String[] args) {
-		BasicConfigurator.configure();
-    	logger.debug("Hello world.");
-    	logger.info("What a beatiful day.");
+	public static void main(String[] args) throws IOException {
+		/* Initialize Logging */
+		Logger logger = Logger.getLogger(Application.class.getName());
+		logger.setLevel(Level.ALL);
+		SimpleLayout sL = new SimpleLayout();
+		ConsoleAppender cA = new ConsoleAppender(sL);
+		logger.addAppender(cA);
+		
+		
+		BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+		boolean quit = false;
+		
+		while (!quit) {
+			System.out.print("Client> ");
+			String input = userInput.readLine();
+			String[] tokens = input.trim().split("\\s+");
+			
+			if (tokens != null) {
+				if (tokens.length == 2  && tokens[0].equals("help") 
+						&& tokens[1].equals("send")) {
+					System.out.println("Syntax: <send> <textmessage>\n"
+							+ "Will send the given message to the currently connected echo server");
+					
+				}
+			}
+		}
+		System.out.println("Hello World");
 	}
 
 }
